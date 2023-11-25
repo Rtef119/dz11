6498c0213bc2d34129794fe3664993a46261c418
rtef119 <rtef119@gmail.com>
2023-11-25 11:08:31 +0100

    dz11

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ cat_face = face_cascade.detectMultiScale(image)
 
 cat = Image.open(image_path)
 glasses = Image.open('glasses.png')
+cmiler = Image.open('2н2й.png')
 cat = cat.convert("RGBA")
 glasses = glasses.convert("RGBA")
+cmiler = cmiler.convert("RGBA")
 for (x, y, w, h) in cat_face:
     glasses = glasses.resize((w, int(h/3)))
     cat.paste(glasses, (x, int(y+h/4)), glasses)
